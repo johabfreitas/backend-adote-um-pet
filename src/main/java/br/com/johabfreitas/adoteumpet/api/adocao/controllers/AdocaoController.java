9753f3c4ba2864d87ffda03aa4d29d3ea7b3b784
Johab Freitas <freitasjohab@gmail.com>
2022-08-19 08:39:27 -0300

    Implementando o JsonNaming

--- src/main/java/br/com/johabfreitas/adoteumpet/api/adocao/controllers/AdocaoController.java
+++ src/main/java/br/com/johabfreitas/adoteumpet/api/adocao/controllers/AdocaoController.java
@@ -3,6 +3,7 @@ package br.com.johabfreitas.adoteumpet.api.adocao.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class AdocaoController {
 
     @PostMapping("/adocoes")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public AdocaoResponse create(AdocaoRequest adocaoRequest) {
+    public AdocaoResponse create(@RequestBody AdocaoRequest adocaoRequest) {
         var adocao = adocaoMapper.toModel(adocaoRequest);
         var createdAdocao = adocaoRepository.save(adocao);
         return adocaoMapper.toResponse(createdAdocao);
--- src/main/java/br/com/johabfreitas/adoteumpet/api/adocao/dtos/AdocaoRequest.java
+++ src/main/java/br/com/johabfreitas/adoteumpet/api/adocao/dtos/AdocaoRequest.java
@@ -2,6 +2,9 @@ package br.com.johabfreitas.adoteumpet.api.adocao.dtos;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +12,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(SnakeCaseStrategy.class)
 public class AdocaoRequest {
     
     private String email;
 
     private BigDecimal valor;
-
+    
     private Long petId;
 }
